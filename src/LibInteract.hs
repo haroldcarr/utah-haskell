90da90b97c0e83f2fa1075b8e74264bfa7d3f55c
Harold Carr <harold.carr@gmail.com>
2015-09-04 10:35:00 -0700

    cleanup

--- src/LibInteract.hs
+++ src/LibInteract.hs
@@ -6,14 +6,13 @@ import qualified Data.Map                as Map
 
 {-
 Created       : 2015 Sep 02 (Wed) 11:56:37 by Harold Carr.
-Last Modified : 2015 Sep 04 (Fri) 10:18:03 by Harold Carr.
+Last Modified : 2015 Sep 04 (Fri) 10:30:46 by Harold Carr.
 -}
 
 newtype Name  = Name  String        deriving (Eq, Ord, Show)
 newtype MsgId = MsgId Int           deriving (Show)
 data    Msg   = Msg   MsgId  String deriving (Show)
 data    In    = In    Name   Msg    deriving (Show)
-data    Out   = Out          Msg    deriving (Show)
 data    User  = User  Name   MsgId  deriving (Show)
 
 challenges :: [(String,String)]
@@ -36,49 +35,54 @@ gp = do
     putMVar mv Map.empty
     return (gu mv, pu mv)
 
-gu :: MVar (Map.Map Name User) -> Name -> IO (Maybe User)
+gu :: Ord k => MVar (Map.Map k a) -> k -> IO (Maybe a)
 gu mv name = do
     m <- takeMVar mv
     let u = Map.lookup name m
     putMVar mv m
     return u
 
-pu :: MVar (Map.Map Name User) -> Name -> User -> IO User
+pu :: Ord k => MVar (Map.Map k b) -> k -> b -> IO b
 pu mv name user = do
     m <- takeMVar mv
     let newM = Map.insert name user m
     putMVar mv newM
     return user
 
-input :: G -> P -> In -> IO Out
+input :: G -> P -> In -> IO Msg
 input gu pu i@(In name _) = do
     r <- gu name
     case r of
         Nothing    -> newUser gu pu i
         Just exist -> existingUser gu pu i exist
 
-newUser      :: G -> P -> In -> IO Out
+newUser :: Monad m => t -> (Name -> User -> m a) -> In -> m Msg
 newUser gu pu (In name _) = do
     let n = 0
     let msgId = MsgId n
     let user = User name msgId
     pu name user
-    return (mkOut n)
+    return (mkMsg n)
 
-mkOut n = Out (Msg (MsgId n) (challenge n))
+mkMsg :: Int -> Msg
+mkMsg n = Msg (MsgId n) (challenge n)
 
-existingUser :: G -> P -> In -> User -> IO Out
-existingUser gu pu (In (Name name) (Msg (MsgId inN) msg)) u@(User _ (MsgId outN)) = do
-    if inN /= outN || msg /= (expect outN) then return (mkOut outN)
-    else do updateUser pu u
+existingUser :: Monad m => t -> (Name -> User -> m a) -> In -> User -> m Msg
+existingUser gu pu   (In (Name name) (Msg (MsgId inN) msg))    u@(User _ (MsgId outN)) =
+    if inN /= outN ||
+       msg /= expect outN
+    then return (mkMsg outN)
+    else updateUser pu u
 
+updateUser :: Monad m => (Name -> User -> m a) -> User -> m Msg
 updateUser pu (User n@(Name name) (MsgId msgId)) = do
     let newId = msgId + 1
     let newMsgId = MsgId newId
     let newUser = User n newMsgId
     pu n newUser
-    return (mkOut newId)
+    return (mkMsg newId)
 
+test :: IO ()
 test = do
     (gu,pu) <- gp
 --    gu (Name "Harold")
