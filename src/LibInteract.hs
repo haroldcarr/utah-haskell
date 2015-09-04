9a4a310e6b9ab7583e9d42efe2be06122eeab819
Harold Carr <harold.carr@gmail.com>
2015-09-04 10:19:44 -0700

    working checkpoint

--- src/LibInteract.hs
+++ src/LibInteract.hs
@@ -6,20 +6,25 @@ import qualified Data.Map                as Map
 
 {-
 Created       : 2015 Sep 02 (Wed) 11:56:37 by Harold Carr.
-Last Modified : 2015 Sep 03 (Thu) 12:00:25 by Harold Carr.
+Last Modified : 2015 Sep 04 (Fri) 10:18:03 by Harold Carr.
 -}
 
-newtype Name  = Name  String                 deriving (Eq, Ord, Show)
-newtype MsgId = MsgId Int                    deriving (Show)
-newtype Msg   = Msg   String                 deriving (Show)
-data In       = In    Name   MsgId      Msg  deriving (Show)
-data Out      = Out   MsgId  Challenges      deriving (Show)
-data User     = User  Name                   deriving (Show)
+newtype Name  = Name  String        deriving (Eq, Ord, Show)
+newtype MsgId = MsgId Int           deriving (Show)
+data    Msg   = Msg   MsgId  String deriving (Show)
+data    In    = In    Name   Msg    deriving (Show)
+data    Out   = Out          Msg    deriving (Show)
+data    User  = User  Name   MsgId  deriving (Show)
 
-type Challenges = [String]
+challenges :: [(String,String)]
+challenges = [ ("foldC1","foldA1")
+             , ("foldC2","foldA2")
+             , ("foldC3","foldA3")
+             ]
 
-challenges :: Challenges
-challenges = ["welcome"]
+challenge = ce fst
+expect    = ce snd
+ce f n = f (challenges!!n)
 
 type G = Name -> IO (Maybe User)
 type P = Name -> User -> IO User
@@ -46,24 +51,41 @@ pu mv name user = do
     return user
 
 input :: G -> P -> In -> IO Out
-input gu pu i@(In name _ _) = do
+input gu pu i@(In name _) = do
     r <- gu name
     case r of
-        Nothing    -> inNew gu pu i
-        Just exist -> inExisting gu pu i exist
+        Nothing    -> newUser gu pu i
+        Just exist -> existingUser gu pu i exist
 
-inNew      :: G -> P -> In -> IO Out
-inNew gu pu (In name _ _) = do
-    let user = User name
+newUser      :: G -> P -> In -> IO Out
+newUser gu pu (In name _) = do
+    let n = 0
+    let msgId = MsgId n
+    let user = User name msgId
     pu name user
-    return (Out (MsgId 0) challenges)
+    return (mkOut n)
 
-inExisting :: G -> P -> In -> User -> IO Out
-inExisting gu pu i u = undefined
+mkOut n = Out (Msg (MsgId n) (challenge n))
 
-{-
-(gu,pu) <- gp
-gu (Name "Harold")
-pu (Name "Harold") (User (Name "Harold"))
-input gu pu (In (Name "Harold") (MsgId 3) (Msg "my name is harold"))
--}
+existingUser :: G -> P -> In -> User -> IO Out
+existingUser gu pu (In (Name name) (Msg (MsgId inN) msg)) u@(User _ (MsgId outN)) = do
+    if inN /= outN || msg /= (expect outN) then return (mkOut outN)
+    else do updateUser pu u
+
+updateUser pu (User n@(Name name) (MsgId msgId)) = do
+    let newId = msgId + 1
+    let newMsgId = MsgId newId
+    let newUser = User n newMsgId
+    pu n newUser
+    return (mkOut newId)
+
+test = do
+    (gu,pu) <- gp
+--    gu (Name "Harold")
+--    pu (Name "Harold") (User (Name "Harold"))
+    one <- input gu pu (In (Name "Harold") (Msg (MsgId (-1)) "my name is harold"))
+    two <- input gu pu (In (Name "Harold") (Msg (MsgId    0) "my name is harold"))
+    thr <- input gu pu (In (Name "Harold") (Msg (MsgId    0) "foldA1"))
+    fou <- input gu pu (In (Name "Harold") (Msg (MsgId    1) "bad"))
+    fiv <- input gu pu (In (Name "Harold") (Msg (MsgId    1) "foldA2"))
+    mapM_ print [one,two,thr,fou,fiv]
