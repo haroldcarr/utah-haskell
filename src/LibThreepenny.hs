84da4b69509a6981d0e78c327992cb60a9ce53a5
Harold Carr <harold.carr@gmail.com>
2015-09-04 12:21:22 -0700

    Now shows history.

--- src/LibThreepenny.hs
+++ src/LibThreepenny.hs
@@ -5,17 +5,37 @@ module LibThreepenny
 )
 where
 
-import           Control.Concurrent     (forkIO)
-import           Graphics.UI.Threepenny
+import           Control.Applicative
+import           Control.Concurrent          (forkIO)
+import           Control.Monad
+import           Data.IORef
+import           Data.Maybe
+import qualified Graphics.UI.Threepenny      as UI
+import           Graphics.UI.Threepenny.Core
 import           Network
-import           System.IO              (hClose)
+import           System.IO                   (hClose)
 
 threepennyMain :: Event String -> IO ()
 threepennyMain event =
-    startGUI defaultConfig $ \win -> do
-        bAccept <- stepper "" event
-        entree <- entry bAccept
-        element entree # set (attr "size") "10" # set style [("width","200px")]
-        getBody win #+ [element entree]
-        return ()
+    startGUI defaultConfig $ \w -> do
+        inputs <- liftIO $ newIORef []
+        let
+            redoLayout :: UI ()
+            redoLayout = void $ do
+                layout <- mkLayout =<< liftIO (readIORef inputs)
+                getBody w # set children [layout]
 
+            mkLayout :: [Element] -> UI Element
+            mkLayout xs = column $ map element xs
+
+            addInput :: String -> UI ()
+            addInput str = do
+                elInput <- UI.input # set value str
+                liftIO $ modifyIORef inputs (elInput:)
+
+            addInputRedoLayout str = do
+                addInput str
+                redoLayout
+
+        onEvent event addInputRedoLayout
+        addInputRedoLayout ""
