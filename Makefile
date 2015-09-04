250cf2dacead6ff1d273eaf813e16f24e03ca2d6
Harold Carr <harold.carr@gmail.com>
2015-09-04 16:12:04 -0700

    getting rid of ununsed stuff

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 #
 # Created       : 2015 Sep 04 (Fri) 14:46:10 by Harold Carr.
-# Last Modified : 2015 Sep 04 (Fri) 15:14:45 by Harold Carr.
+# Last Modified : 2015 Sep 04 (Fri) 16:10:32 by Harold Carr.
 #
 
 b build : FORCE
@@ -10,10 +10,8 @@ r run : FORCE
 	stack exec utah-haskell-exe
 
 t test : FORCE
-	curl 127.0.0.1:3000/foo
-	curl 127.0.0.1:3000/bar
-	curl 127.0.0.1:3000/baz
-	curl 127.0.0.1:3000/reg?email=harold -d '{ "json" : "format" }'
+	curl 127.0.0.1:3000
+	curl 127.0.0.1:3000 -d '{ "json" : "format" }'
 
 c clean : FORCE
 	stack clean
--- src/LibScotty.hs
+++ src/LibScotty.hs
@@ -26,19 +26,7 @@ instance FromJSON User
 
 scottyMain :: Handler String -> IO ()
 scottyMain handler = scotty 3000 $ do
-    get "/users" $ do
-        liftIO (handler "/users")
-        json allUsers
-    get "/users/:id" $ do
-        id <- param "id"
-        liftIO (handler ("/users/" ++ show id))
-        json $ filter ((==id) . userId) allUsers
-    post "/reg" $ do
-        e <- param "email" `rescue` const next
+    post "/" $ do
         b <- body
-        liftIO (handler ("/reg " ++ show e ++ " body: " ++ unpack (decodeUtf8 b)))
-        html $ mconcat [ "ok ", e ]
-    get "/:word" $ do
-        beam <- param "word"
-        liftIO (handler ("/* " ++ show beam))
-        html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+        liftIO (handler (unpack (decodeUtf8 b)))
+        json allUsers
