9696ddfdbeb337361ed0a2224acc907c8ea48f50
Harold Carr <harold.carr@gmail.com>
2015-09-14 11:20:40 -0700

    Update lts version and other cleanups based on Sean Hess feedback.

--- 2015-09-17-template/Client.hs
+++ 2015-09-17-template/Client.hs
@@ -1,8 +1,21 @@
+{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Client where
 
 import           Control.Lens
-import           Data.Aeson   (toJSON)
-import           Msg
+import           Data.Aeson      (FromJSON, ToJSON, decode, toJSON)
+import           Data.Aeson.Lens (key)
+import           Data.Maybe      (fromJust)
+import           GHC.Generics
 import           Network.Wreq
+
+type Name  = String
+type MsgId = Int
+data Msg   = Msg { name  :: Name
+                 , msgId :: MsgId
+                 , txt   :: String
+                 } deriving (Generic, Show)
+
+instance ToJSON   Msg
+instance FromJSON Msg
