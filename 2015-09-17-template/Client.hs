1f6cf4572ebb36c45d3d5d0cccfa0e1a0e88af2f
Harold Carr <harold.carr@gmail.com>
2015-09-13 18:56:18 -0700

    created 2015-09-17-template

---
+++ 2015-09-17-template/Client.hs
@@ -0,0 +1,8 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Client where
+
+import           Control.Lens
+import           Data.Aeson   (toJSON)
+import           Msg
+import           Network.Wreq
